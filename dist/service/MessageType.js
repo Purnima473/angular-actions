b33467f3d6fd1c7126201273a055b2592f7df81f
joegorilla <joe.gorilla@buildmotion.com>
2017-03-26 21:53:47 -0600

    initial commit for the npm dist folder;

---
+++ dist/service/MessageType.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Use to indicate the type for the [ServiceMessage].
+ */
+var MessageType;
+(function (MessageType) {
+    /**
+     * Use to indicate the message type is informational.
+     */
+    MessageType[MessageType["Information"] = 1] = "Information";
+    /**
+     * Use to indicate the message type is warning.
+     */
+    MessageType[MessageType["Warning"] = 2] = "Warning";
+    /**
+     * Use to indicate the message type is error.
+     */
+    MessageType[MessageType["Error"] = 3] = "Error";
+})(MessageType = exports.MessageType || (exports.MessageType = {}));
+//# sourceMappingURL=/service/MessageType.js.map
